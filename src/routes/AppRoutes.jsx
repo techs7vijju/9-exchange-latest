de5698a142f20d571fa150630d624713c88d82c2
techs7vijju <vijayalakshmi@sseventech.com>
2025-07-22 14:28:05 +0530

    completed structure

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Route, Routes, Outlet } from "react-router-dom";
 import { useMediaQuery } from "react-responsive";
-import Header from "../layout/header/Header";
 import Home from "../pages/home/Homepage";
 import Appui from "../pages/home/Appui";
 import Layout from "../layout/Layout";
+import Tickets from "../pages/more/tickets/tickets";
+import MoreHome from "../pages/more/moreHome";
 
 function AppRoutes() {
   const isMobile = useMediaQuery({ maxWidth: 767 });
@@ -14,6 +15,15 @@ function AppRoutes() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/more" element={<MoreHome />}>
+            <Route index element={<DefaultMoreContent />} />
+            {/* <Route path="deposits" element={<Deposits />} /> */}
+            {/* <Route path="withdrawals" element={<Withdrawals />} /> */}
+            <Route path="tickets" element={<Tickets />} />
+            {/* <Route path="profile" element={<Profile />} /> */}
+            {/* <Route path="privacy-policy" element={<PrivacyPolicy />} /> */}
+            {/* <Route path="terms-of-service" element={<TermsOfService />} /> */}
+          </Route>
         </Route>
 
         <Route path="/appui" element={<Appui />} />
@@ -21,14 +31,11 @@ function AppRoutes() {
     </div>
   );
 }
-
-function LayoutWithHeader() {
+function DefaultMoreContent() {
   return (
-    <>
-      <Header />
-      <Outlet />
-    </>
+    <div className="w-80 h-100 grey-color1-bg">
+      Select an option from the sidebar
+    </div>
   );
 }
-
 export default AppRoutes;
