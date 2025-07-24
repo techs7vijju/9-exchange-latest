579dd56e081d6934db2284d54028de98a53a9ec0
hanu@9490 <hanumanthu@sseven.com>
2025-07-24 18:22:31 +0530

    changes

--- src/layout/mobile/components/MobileHeader.jsx
+++ src/layout/mobile/components/MobileHeader.jsx
@@ -1,26 +1,29 @@
-import React from 'react'
-import { FaSearch, FaStar } from 'react-icons/fa'
-import { IoArrowBack } from 'react-icons/io5'
-import { RiSearch2Line } from 'react-icons/ri'
+import React from "react";
+import { FaSearch, FaStar } from "react-icons/fa";
+import { IoArrowBack } from "react-icons/io5";
+import { RiSearch2Line } from "react-icons/ri";
 
 const MobileHeader = () => {
   return (
-    <div className='mobile-header-container p-2'>
-     <div className='mobile-header-content'>
-        <div className='mobile-content-left mx-1'>
-        <IoArrowBack className='icon' size={45} />
-        <p>SPORTS</p>
+    <div className="mobile-header-container p-2">
+      <div className="mobile-header-content">
+        <div className="mobile-content-left mx-1">
+          <div className="flex-center icon-wrapper">
+            <IoArrowBack className="icon" size={20} />
+          </div>
+          <p>SPORTS</p>
         </div>
-        <div className='mobile-content-right mx-1'>
-        <FaSearch
-
-        className='icon' size={45} />
-       <FaStar
-       className='icon' size={45} />
+        <div className="mobile-content-right mx-1">
+          <div className="flex-center icon-wrapper">
+            <FaSearch className="icon" size={20} />
+          </div>
+          <div className="flex-center icon-wrapper">
+            <FaStar className="icon" size={20} />
+          </div>
         </div>
-     </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default MobileHeader
+export default MobileHeader;
