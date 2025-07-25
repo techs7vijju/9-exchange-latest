b8a46eaa79c0f86ba4bfd3234f57bc0be9f4f807
hanu@9490 <hanumanthu@sseven.com>
2025-07-25 13:15:10 +0530

    changes

--- src/layout/SportsLayout.jsx
+++ src/layout/SportsLayout.jsx
@@ -10,11 +10,11 @@ const SportsLayout = () => {
   const [activeTab, setActiveTab] = useState("cricket");
   const showTabs = isMobile && location.pathname.startsWith("/sports");
   return (
-    <div className="w-100 d-flex h-100 flex-1">
+    <div className="w-100 d-flex h-100 flex-1 p-2">
       {showTabs && (
         <SportsTabNavigator activeTab={activeTab} onTabChange={setActiveTab} />
       )}
-      <div style={{ flex: "1" }}>
+      <div style={{ flex: 1, overflowY: "auto" }}>
         <Outlet context={{ activeTab }} />
       </div>
       <div className="d-none d-lg-block">
